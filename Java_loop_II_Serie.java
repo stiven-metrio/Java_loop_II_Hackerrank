a333f95d2e3504688ed5b6a841b02299d3549baf
Jhonatan <jhonatanmetrio@gmail.com>
2019-01-16 12:56:38 -0500

    Reads t, a, b & n

---
+++ Java_loop_II_Serie.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Java_loop_II_Serie {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		// Amount of series
+		int t = in.nextInt();
+		
+		// a, b & n for the series
+		int a, b, n;
+		for (int i = 0; i < t; i++) {
+			a = in.nextInt();
+			b = in.nextInt();
+			n = in.nextInt();
+		}
+		in.close();
+				
+
+	}
+
+}

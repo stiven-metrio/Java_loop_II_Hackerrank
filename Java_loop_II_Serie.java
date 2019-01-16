0f9dfd2b52276cad2f318582196860b505b7f955
Jhonatan <jhonatanmetrio@gmail.com>
2019-01-16 14:06:17 -0500

    Calculate and print serie

--- Java_loop_II_Serie.java
+++ Java_loop_II_Serie.java
@@ -20,6 +20,24 @@ public class Java_loop_II_Serie {
 			abnArray[i][2] = n;
 		}
 		in.close();
+
+		// print abnArray
+		for (int i = 0; i < amountSeries; i++) {
+			System.out.printf("%d\t%d\t%d%n",abnArray[i][0],abnArray[i][1],abnArray[i][2]);
+		}
+		
+		// Print series
+		int temp;
+		for (int j = 0; j < amountSeries; j++) {
+			temp = abnArray[j][0] + abnArray[j][1]; // a + b
+			System.out.printf("%d ",temp);
+			for (int i = 1; i < abnArray[j][2]; i++) {
+				temp = (int) (temp + Math.pow(2, i)*abnArray[j][1]);
+				System.out.printf("%d ",temp);
+			}
+			System.out.println();
+		}
+		
 	}
 
 }

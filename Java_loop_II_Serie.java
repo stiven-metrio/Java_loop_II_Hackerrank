023f1d28b3e129458631ebb189e9f9aa2f98a1e4
Jhonatan <jhonatanmetrio@gmail.com>
2019-01-16 13:05:34 -0500

    Save array with a b n values

--- Java_loop_II_Serie.java
+++ Java_loop_II_Serie.java
@@ -5,19 +5,21 @@ public class Java_loop_II_Serie {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		
-		// Amount of series
-		int t = in.nextInt();
+		int amountSeries = in.nextInt();
+		int abnArray[][] = new int[amountSeries][3];
 		
 		// a, b & n for the series
 		int a, b, n;
-		for (int i = 0; i < t; i++) {
+		for (int i = 0; i < amountSeries; i++) {
 			a = in.nextInt();
 			b = in.nextInt();
 			n = in.nextInt();
+			// Save array with a b n values
+			abnArray[i][0] = a;
+			abnArray[i][1] = b;
+			abnArray[i][2] = n;
 		}
 		in.close();
-				
-
 	}
 
 }
